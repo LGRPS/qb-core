0d969b8a21e07f810c3926bf63d5199a2f34e32f
WRIGH7Y <92814197+WRIGH7Y@users.noreply.github.com>
2021-10-30 15:56:12 +0100

    Update shared.lua
    added crafting table items

--- shared.lua
+++ shared.lua
@@ -422,6 +422,13 @@ QBShared.Items = {
 	['stripedbass'] 			     = {['name'] = 'stripedbass', 			        ['label'] = 'Striped Bass',             ['weight'] = 1111,      ['type'] = 'item',      ['image'] = 'stripedbass.png',          ['unique'] = false,     ['useable'] = true,     ['shouldClose'] = true,    ['combinable'] = nil,   ['description'] = 'A Striped Bass'},
 	['fishingrod'] 			 		 = {['name'] = 'fishingrod', 					['label'] = 'Fishing Rod', 				['weight'] = 5000, 		['type'] = 'item', 		['image'] = 'fishingrod.png', 			['unique'] = false, 	['useable'] = true, 	['shouldClose'] = true,	   ['combinable'] = nil,   ['description'] = 'A fishing rod for adventures with friends!!'},
 	['fishingbait'] 			     = {['name'] = 'fishingbait', 					['label'] = 'Fish Bait', 				['weight'] = 400, 		['type'] = 'item', 		['image'] = 'fishbait.png', 			['unique'] = false, 	['useable'] = true, 	['shouldClose'] = true,	   ['combinable'] = nil,   ['description'] = 'A fish bait'},
+
+
+    ['craftingtable'] 			     = {['name'] = 'craftingtable', 				['label'] = 'Crafting Table', 			['weight'] = 400, 		['type'] = 'item', 		['image'] = 'fishbait.png', 			['unique'] = true, 	    ['useable'] = false, 	['shouldClose'] = false,   ['combinable'] = nil,   ['description'] = ''},
+    ['recipe_bagofdope'] 			 = {['name'] = 'recipe_bagofdope', 				['label'] = 'Recipe: Bag of Dope', 		['weight'] = 400, 		['type'] = 'item', 		['image'] = 'fishbait.png', 			['unique'] = false, 	['useable'] = false, 	['shouldClose'] = false,   ['combinable'] = nil,   ['description'] = ''},
+    ['bagofdope'] 			         = {['name'] = 'bagofdope', 					['label'] = 'Bag of Dope', 				['weight'] = 400, 		['type'] = 'item', 		['image'] = 'fishbait.png', 			['unique'] = false, 	['useable'] = false, 	['shouldClose'] = false,   ['combinable'] = nil,   ['description'] = ''},
+    ['weed'] 			             = {['name'] = 'weed', 					        ['label'] = 'Weed', 				    ['weight'] = 400, 		['type'] = 'item', 		['image'] = 'fishbait.png', 			['unique'] = false, 	['useable'] = false, 	['shouldClose'] = false,   ['combinable'] = nil,   ['description'] = ''},
+    ['drugscales'] 			         = {['name'] = 'drugscales', 					['label'] = 'Scales', 				    ['weight'] = 400, 		['type'] = 'item', 		['image'] = 'fishbait.png', 			['unique'] = false, 	['useable'] = false, 	['shouldClose'] = false,   ['combinable'] = nil,   ['description'] = ''},
 }
 
 -- // HASH WEAPON ITEMS, NEED SOMETIMES TO GET INFO FOR CLIENT
