849eba9b355cf4f9bd8c7136a1e9d51237da05a3
WRIGH7Y <92814197+WRIGH7Y@users.noreply.github.com>
2021-11-10 14:13:30 +0000

    Update loops.lua

--- client/loops.lua
+++ client/loops.lua
@@ -12,12 +12,11 @@ CreateThread(function()
     while true do
         Wait(QBCore.Config.StatusInterval)
         if LocalPlayer.state['isLoggedIn'] then
-            if not QBCore.Functions.GetPlayerData().metadata['inlaststand'] and not QBCore.Functions.GetPlayerData().metadata['isdead'] then
+            if not QBCore.Functions.GetPlayerData().metadata['inlaststand'] then
                 if QBCore.Functions.GetPlayerData().metadata['hunger'] <= 0 or QBCore.Functions.GetPlayerData().metadata['thirst'] <= 0 then
                     local ped = PlayerPedId()
                     local currentHealth = GetEntityHealth(ped)
-                    --SetEntityHealth(ped, currentHealth - math.random(5, 10))
-                    SetEntityHealth(ped, currentHealth - math.random(1, 3))
+                    SetEntityHealth(ped, currentHealth - math.random(5, 10))
                 end
             end
         end
